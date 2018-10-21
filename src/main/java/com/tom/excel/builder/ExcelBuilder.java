98180fb828b8a27b5186c55931061a0d323a71bb
Sandyli <sandyli@192.168.1.103>
2018-10-21 16:05:01 +0800

    [ADD] create ExcelBuilder

--- src/main/java/com/tom/excel/builder/ExcelBuilder.java
+++ src/main/java/com/tom/excel/builder/ExcelBuilder.java
@@ -1,5 +1,11 @@
 package com.tom.excel.builder;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 /**
  * Base Excel Builder
  *
@@ -8,4 +14,29 @@ package com.tom.excel.builder;
  * @since v1.0.0
  */
 public class ExcelBuilder<T> {
+
+    private final Supplier<T> instant;
+
+    private List<Consumer<T>> instantModified = new ArrayList<>(8);
+
+    public ExcelBuilder(Supplier<T> supplier) {
+        instant = supplier;
+    }
+
+    public static <T> ExcelBuilder<T> of(Supplier<T> supplier) {
+        return new ExcelBuilder<T>(supplier);
+    }
+
+    public <U> ExcelBuilder<T> with(BiConsumer<T,U> biConsumer, U value) {
+        Consumer<T> consumer = instant -> biConsumer.accept(instant,value);
+        instantModified.add(consumer);
+        return this;
+    }
+
+    public T build() {
+        T value = instant.get();
+        instantModified.forEach(consumer -> consumer.accept(value));
+        instantModified.clear();
+        return value;
+    }
 }
