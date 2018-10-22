de0b15acd0ae12f08fb5f3166370470d39a4ab5e
tomxin <863970612@qq.com>
2018-10-23 07:58:28 +0800

    [ADD] ExcelException

--- src/main/java/com/tom/excel/exceptions/ExcelException.java
+++ src/main/java/com/tom/excel/exceptions/ExcelException.java
@@ -9,4 +9,39 @@ package com.tom.excel.exceptions;
  */
 public class ExcelException extends RuntimeException {
 
+    /**
+     * 默认无参数的构造函数
+     */
+    public ExcelException() {
+        super();
+    }
+
+    /**
+     * 带参数的构造函数
+     *
+     * @param message
+     */
+    public ExcelException(String message) {
+        super(message);
+    }
+
+    /**
+     * 带参数的构造函数
+     *
+     * @param message
+     * @param e
+     */
+    public ExcelException(String message,Throwable e) {
+        super(message,e);
+    }
+
+    /**
+     * 带参数的构造函数
+     *
+     * @param e
+     */
+    public ExcelException(Throwable e) {
+        super(e);
+    }
+
 }
