ab744c7ab2007eabb9e1f5b516ac624993b0e49f
tomxin <863970612@qq.com>
2018-10-23 08:05:28 +0800

    [ADD] Excel Exception Factory

---
+++ src/main/java/com/tom/excel/exceptions/ExcelExceptionFactory.java
@@ -0,0 +1,30 @@
+package com.tom.excel.exceptions;
+
+/**
+ * Exception Factory
+ *
+ * @author tomxin
+ * @date 2018-10-23
+ * @since v1.0.0
+ */
+public class ExcelExceptionFactory {
+
+    /**
+     * default construct method
+     *
+     */
+    public ExcelExceptionFactory() {
+
+    }
+
+    /**
+     * wrap exception
+     *
+     * @param message
+     * @param exception
+     * @return
+     */
+    public static RuntimeException wrapException(String message, Exception exception) {
+        return new ExcelException(message,exception);
+    }
+}
